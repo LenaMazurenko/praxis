28f76e5b0212d894617e666031300a63a5ae9476
LenaMazurenko <lena@mazurenko.de>
2023-03-22 12:39:24 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Praxis Wentland
+                    Praxis Wentlandd
                 </a>
             </header>
         </div>
