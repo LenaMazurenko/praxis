04512546d7f082568e732f5648ab6bb01671a1f8
LenaMazurenko <lena@mazurenko.de>
2023-03-26 14:04:05 +0300

    start files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Routes, Route } from "react-router-dom";
-import { Home } from "../src/Pages/Home";
-import { About } from "../src/Pages/About";
-import { Salon } from "../src/Pages/Salon";
-import { Service } from "../src/Pages/Service";
-import { Contacts } from "../src/Pages/Contacts";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/salon" element={<Salon />} />
-            <Route path="/service" element={<Service />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contacts />} />
-        </Routes>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/Pages/Salon.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Salon = () => {
-    return <div>Salon</div>;
-};
